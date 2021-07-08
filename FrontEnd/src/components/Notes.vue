71ffb7bdc8293e01ceceb1cd8ca2af6005526851
Lan Sevcnikar <lan.sevcnikar@druga.si>
2021-07-08 13:29:20 +0200

    This is some more code

---
+++ FrontEnd/src/components/Notes.vue
@@ -0,0 +1,60 @@
+<template>
+  <div style="background-color: red">
+    <p>Colleges</p>
+    <router-link to="/notes/new"> Add College </router-link>
+    <router-link
+      v-for="(coll, index) in notes"
+      :key="index"
+      :to="'/notes/edit/'+coll.id"
+    >
+      <note
+        :coll="coll"
+      ></note>
+    </router-link>
+  </div>
+</template>
+
+<script>
+import Note from "./Note.vue";
+
+export default {
+  components: {
+    Note,
+  },
+  data() {
+    return {
+      notes: [
+        {
+          id: "col-101",
+          name: "AMU",
+          location: "Uttar Pradesh",
+          rating: 5.0,
+        },
+        {
+          id: "col-102",
+          name: "CUSAT",
+          location: "Kerala",
+          rating: 4.5,
+        },
+        {
+          id: "col-103",
+          name: "Harvard",
+          location: "Sometwhere",
+          rating: 4.8,
+        },
+      ],
+    };
+  },
+  methods: {
+    edit(id) {
+      console.log(id);
+    },
+    allNotes(){
+      return this.notes;
+    }
+  },
+};
+</script>
+
+<style>
+</style>
--- FrontEnd/src/main.js
+++ FrontEnd/src/main.js
@@ -4,7 +4,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App.vue'
 import {store} from './store'
 import router from './router';
+import GoogleAuth from 
 const app  = createApp(App)
 app.use(router);
 app.use(store);
+app.use(GoogleAuthg, gauthOption);
+app.use(DKToast, toastOptions);
+app.use(Toast);
 app.mount('#app')
