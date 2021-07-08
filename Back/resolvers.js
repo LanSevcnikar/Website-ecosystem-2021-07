9f0e54570bcfcd24a0a377c9c4419f59d50e8ffb
Lan Sevcnikar <lan.sevcnikar@druga.si>
2021-07-08 08:48:20 +0200

    Added a mutations method

--- Back/resolvers.js
+++ Back/resolvers.js
@@ -3,11 +3,29 @@ const Query = {
   greeting: () => {
     return "hello from  TutorialsPoint !!!";
   },
-  sayHello:(root,args,context,info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
+  sayHello: (root, args, context, info) => `Hi ${args.name} GraphQL server says Hello to you!!`,
   students: () => db.students.list(),
   studentById: (root, args, context, info) => {
     return db.students.get(args.id);
   },
+};
+
+const Mutation = {
+  createStudentReturn: (root, args, context, info) => {
+    const id = db.students.create({
+      collegeId: args.collegeId,
+      firstName: args.firstName,
+      lastName: args.lastName
+    });
+    return db.students.get(id)
+  },
+  createStudent: (root, args, context, info) => {
+    return db.students.create({
+      collegeId: args.collegeId,
+      firstName: args.firstName,
+      lastName: args.lastName
+    })
+  }
 };
 
 const Student = {
@@ -19,4 +37,4 @@ const Student = {
   },
 };
 
-module.exports = { Query, Student };
+module.exports = { Query, Mutation, Student };
