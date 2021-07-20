b99e730b99cedcc5cc0734b0a2fb6309f89f92e8
LanS <lan.sevcnikar@druga.si>
2021-07-20 21:19:53 +0200

    This was pretty fun to make, everything works and I love Hasura

--- Client/src/views/CaS.vue
+++ Client/src/views/CaS.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="m-auto">
       <jwt></jwt>
+      <greeting-card> </greeting-card>
       <college-card> </college-card>
       <students-card></students-card>
     </div>
@@ -9,12 +10,14 @@
 </template>
 
 <script>
+import GreetingCard from "../components/GreetingCard.vue";
 import CollegeCard from "../components/CollegeCard.vue";
 import StudentsCard from "../components/StudentsCard.vue";
 import jwt from "../components/JWT.vue";
 
 export default {
   components: {
+    GreetingCard,
     CollegeCard,
     StudentsCard,
     jwt,
